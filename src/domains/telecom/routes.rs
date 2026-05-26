445aef94ec8c033db3ecc4c4746a48495bd9a669
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-05-26 09:34:49 +0100

    Mount number pricing route

--- src/domains/telecom/routes.rs
+++ src/domains/telecom/routes.rs
@@ -22,6 +22,7 @@ pub fn router() -> Router<AppState> {
             "/telecom/numbers/search",
             get(inventory::search_global_numbers),
         )
+        .route("/telecom/numbers/pricing", get(inventory::list_number_pricing))
         .route("/telecom/numbers/mine", get(inventory::list_my_numbers))
         .route(
             "/telecom/numbers/reserve",
