a3c3af640d093f1d03ee244c65aa9c66fd15fd50
Bkzazzau1 <ibrahim.yahaya@miva.edu.ng>
2026-05-28 11:26:20 +0100

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -28,9 +28,9 @@ impl AppState {
         let cache = cache::connect(&config.redis_url).await?;
         let solana_rpc = Arc::new(TreasuryRpc::new(config.solana_rpc_url.clone()));
         let trading_co_wallet = Arc::new(TradingWallet::new(
-    config.trading_co_treasury.clone(),
-    config.trading_company_second_wallet.clone(),
-));
+            config.trading_co_treasury.clone(),
+            config.trading_company_second_wallet.clone(),
+            ));
         let http = reqwest::Client::new();
 
         Ok(Self {
